cd9fd6ddd13d7cf0e3c7cb4238d18872626c47d4
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-02-02 17:06:03 -0300

    fix:createTables.sql fix

--- createTables.sql
+++ createTables.sql
@@ -1,3 +1,7 @@
- DELETE FROM movies WHERE id = 3; 
-
- UPDATE movies SET "movieName" = 'Miranha de volta ao lar' WHERE id = 1; 
+CREATE TABLE IF NOT EXISTS movies (
+  id BIGSERIAL PRIMARY KEY,
+  "movieName" VARCHAR(50) UNIQUE NOT NULL,
+  "movieDescription" TEXT,
+  "movieDuration" INTEGER NOT NULL,
+  "moviePrice" INTEGER NOT NULL
+);
