c3851266489d2c0bff2ae9969beb2514364435eb
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-02-02 12:58:11 -0300

    feat: final config

--- createTables.sql
+++ createTables.sql
@@ -1,8 +1,3 @@
-DELETAR itens => DELETE FROM movies WHERE id = 3; 
+ DELETE FROM movies WHERE id = 3; 
 
-Editar itens => UPDATE movies SET "movieName" = 'Miranha de volta ao lar' WHERE id = 1; 
-
-UPDATE clientes
-SET
-  (nome, endereco, preco) = ROW('Fulano', 'Rua do Sol', preco * 1.1)
-WHERE id = 1;
+ UPDATE movies SET "movieName" = 'Miranha de volta ao lar' WHERE id = 1; 
